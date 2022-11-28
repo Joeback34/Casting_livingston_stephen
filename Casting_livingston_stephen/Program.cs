8a84cc43ba548bb0ef84fad7c2fec18ca53b2b29
Joeback34 <joebackthehero@gmail.com>
2022-11-28 13:48:34 -0600

    Initial commit

---
+++ Casting_livingston_stephen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Casting_livingston_stephen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
